7b6cb9b5911872a4ed45d7bfa8e9d6cc9a5109b2
S3yon <sriskans@sheridancollege.ca>
2025-07-16 14:26:45 -0400

    feat: add favicon link to enhance site branding

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -113,6 +113,7 @@ export default function ClientLayout({
         <meta name="msapplication-TileColor" content="#0a0a0a" />
         <meta name="msapplication-tap-highlight" content="no" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </head>
